d64cfc81186b56dfb17c88e0827820a368485d91
DoggySazHi <william.le.huy@gmail.com>
2021-03-31 15:05:12 -0700

    WIP2 for main screen

--- StaticViews/js/index.js
+++ StaticViews/js/index.js
@@ -2,27 +2,32 @@
 
 onIndexInit();
 
-let header = document.getElementsByTagName("header")[0];
-let text = header.getElementsByTagName("span")[0];
-let cursor = header.getElementsByTagName("span")[1];
-
 let status = 0;
 
 function onIndexInit() {
-    setInterval(changeText, 100)
+    setInterval(changeText, 60)
     setInterval(blinkCursor, 500)
+    addEventListener("resize", computeHeight);
+    computeHeight();
+
+    console.info("Initialized index script!");
 }
 
 let blink = false;
 
-let words = ["Hi.", " I'm William Le."]
+const words = ["Hi.", " I'm William Le."]
 let charIndex = 0;
 let wordIndex = 0;
-let delay = 0;
+let delay = 10;
 
 function changeText() {
-    if (wordIndex === words.length)
+    let header = document.getElementsByTagName("header")[0];
+    let text = header.getElementsByTagName("span")[0];
+    
+    if (wordIndex >= words.length) {
+        reveal();
         return;
+    }
     
     if (delay !== 0) {
         --delay;
@@ -34,13 +39,16 @@ function changeText() {
     if (charIndex === words[wordIndex].length) {
         charIndex = 0;
         ++wordIndex;
-        delay = 20;
+        delay = 15;
         status = 0;
         blinkCursor();
     }
 }
 
 function blinkCursor() {
+    let header = document.getElementsByTagName("header")[0];
+    let cursor = header.getElementsByTagName("span")[1];
+
     if (status === 1)
         blink = true;
     
@@ -49,4 +57,24 @@ function blinkCursor() {
     else
         cursor.innerHTML = "";
     blink = !blink;
+}
+
+function reveal() {
+    let classes = document.getElementById("content").classList;
+    classes.remove("covered");
+    classes.add("reveal");
+}
+
+function computeHeight() {
+    let header = document.getElementsByTagName("header")[0];
+    
+    header.classList.add("hidden");
+    let temp = header.innerHTML;
+    header.innerHTML = "";
+    for (let i of words)
+        header.innerHTML += i;
+    let out = header.offsetHeight;
+    header.innerHTML = temp;
+    header.style.height = out;
+    header.classList.remove("hidden");
 }
